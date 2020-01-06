84e5f5857d0153e124ab9e3d4e41c33afe22b106
hyuk <pjh08190819@afreecatv.com>
2020-01-06 16:59:03 +0900

    echo framework setting

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 
 func main() {
-    fmt.Printf("hello\n");
+	e := echo.New()
+
+	//HTTP Error Handler
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
+	//middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+	}
+	errorPage := fmt.Sprintf("errorHtml/%d.html", code)
+	if err := c.File(errorPage); err != nil {
+		c.Logger().Error(err)
+	}
+	// c.Logger().Error(err)
 }
