236913c8b5a1a9df00d468beb6b78fb1eff8a7b1
hyuk <pjh08190819@afreecatv.com>
2020-01-16 20:20:49 +0900

    자동완성 기능 추가

--- front/src/components/AfHeader.vue
+++ front/src/components/AfHeader.vue
@@ -12,18 +12,36 @@
         </h3>
         <fieldset>
           <legend>검색 폼</legend>
-          <input type="text" v-model="keyword" name="szKeyword" id="szKeyword" title="검색어 입력" class="search_bar" @keyup="autoJaso">
-          <a href="javascript:;" title="검색" class="btn_search" @click="search">검색</a>
+          <input type="text" v-model="keyword" name="szKeyword" id="szKeyword" title="검색어 입력" class="search_bar" @keyup="autoJaso" @click=checkLive(keyword)>
+          <a href="javascript:;" title="검색" class="btn_search" @click="search(keyword)">검색</a>
           <!-- 자동 완성 -->
           <div class="sear_auto" id="divSearchAuto" v-if="auto">
-            <p>몰라이씨</p>
+            <ul>
+              <li v-for="oData in list" v-bind:key="oData">
+                <a :href="searchUrl(oData.d)">{{oData.d}}</a>
+              </li>
+            </ul>
           </div>
           <!-- 최근 검색어 -->
           <div class="mysch" v-if="mysch">
             {{ list }}
           </div>
           <!-- 실시간 검색어 -->
-          <div class="livesch" style="display:none;"></div>
+          <div class="livesch" v-if="live">
+            <p class="stitle">실시간 인기 검색어</p>
+            <ul  v-for="(oData, nIndex) in list" v-bind:key="oData">
+              <li>
+                <a :href="searchUrl(oData.keyword)">
+                <em>{{ (nIndex+1) }}</em>
+                <span class="tit">{{ oData.keyword }}</span>
+                <span class="rank">
+                  <span :class="liveKeywordList(oData.updown, oData.show_text)">{{ oData.show_text }}</span>
+
+                </span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </fieldset>
       </div>
   </div>
@@ -35,36 +53,86 @@ export default {
   data () {
     return {
       msg: '재원공주다',
-      list: '123',
+      list: '',
       mysch: false,
-      auto: true,
+      auto: false,
+      live: false,
       keyword: ''
     }
   },
   methods: {
     autoJaso (e) {
+      let szKeyword = e.target.value
+      if (!szKeyword) {
+        this.checkLive()
+        return false
+      }
       // 방향키
       var aDirectionCode = [37, 38, 30, 40]
       if (aDirectionCode.includes(e.which)) {
-        console.log('key')
       } else if (e.which === 13) {
         location.href = '//search.afreecatv.com?keyword=' + e.target.value
       } else {
-        this.$http.get('http://203.238.139.11:8080/test').then((res) => {
-          console.log('result:', res.data)
-          this.list = res.data
-          this.mysch = true
+        let params = {}
+        params['m'] = 'autoJaso'
+        params['v'] = '1.0'
+        params['t'] = 'json'
+        params['c'] = 'UTF-8'
+        params['d'] = e.target.value
+        params['n'] = 10
+        params['w'] = 'webk'
+
+        this.$http.get('//scketc.afreecatv.com/api.php', {
+          params: params
+        }).then((res) => {
+          this.list = res.data.list
+          this.live = false
+          this.auto = true
         })
       }
     },
-    search () {
-      location.href = '//search.afreecatv.com?keyword=' + 'value'
+    search (e) {
+      location.href = '//search.afreecatv.com?keyword=' + e
     },
-    setting() {
+    setting () {
       console.log('이건 머야')
+    },
+    searchUrl (szKeyword) {
+      return '?keyword=' + szKeyword
+    },
+    checkLive (szKeyword) {
+      if (szKeyword) {
+
+      } else {
+        let params = {}
+        params['m'] = 'hotKeyword'
+        params['v'] = '1.0'
+        params['t'] = 'json'
+        params['c'] = 'UTF-8'
+
+        this.$http.get('//scketc.afreecatv.com/api.php', {
+          params: params
+        }).then((res) => {
+          this.list = res.data.HOT
+          this.auto = false
+          this.live = true
+        })
+      }
+    },
+    liveKeywordList (szUpdown, szShowText) {
+      if (szUpdown === 'up' || szUpdown === 'down') {
+        return szUpdown
+      } else {
+        if (szShowText === 'new') {
+          return szShowText
+        } else {
+          return szShowText
+        }
+      }
     }
+
   },
-  created: function() {
+  created: function () {
     this.setting()
   }
 }
